d14cd2dc89ccba5ba6171321f55bf25ec9b8bbff
Will Harris <willmharris7@gmail.com>
2021-10-18 01:11:55 -0700

    all backend tests done

--- backend/src/__tests__/route_tests.ts
+++ backend/src/__tests__/route_tests.ts
@@ -19,11 +19,14 @@ describe('Test endpoints', function() {
         expect(response.status).toBe(200)
         expect(response.body.express).toBe('I received your POST request. This is what you sent me: john')
     })
+})
 
-    it('returns the list of databases from MonboDB Atlas', async () => {
+
+describe('Database endpoints', function() {
+
+    it('returns the list of databases in MonboDB Atlas from /api/dbs', async () => {
         const response = await request.get('/api/dbs')
         expect(response.status).toBe(200)
-        console.log(response.body.express)
         expect(response.body.express).toStrictEqual([
             'sample_airbnb',
             'sample_analytics',
@@ -37,23 +40,33 @@ describe('Test endpoints', function() {
             'local'
         ])
     })
+
+    it('returns the top 5 listings in the MongoDB database from /api/top5', async () => {
+        const response = await request.get('/api/top5')
+        expect(response.status).toBe(200)
+        expect(response.body.express).toStrictEqual([
+            'Ribeira Charming Duplex has 3 bedrooms',
+            'Horto flat with small garden has 1 bedrooms',
+            'Ocean View Waikiki Marina w/prkg has 1 bedrooms',
+            'Private Room in Bushwick has 1 bedrooms',
+            'Apt Linda Vista Lagoa - Rio has 1 bedrooms'
+        ])
+    })
+
+    it('returns results with 3 bedrooms given the post body of 3 from /api/bedrooms', async () => {
+        const response = await request
+            .post('/api/bedrooms')
+            .send({test_key: '3'})
+            .set('Accept', 'application/json')
+        expect(response.status).toBe(200)
+        expect(response.body.express).toStrictEqual([
+            'Ribeira Charming Duplex has 3 bedrooms',
+            '3 chambres au coeur du Plateau has 3 bedrooms',
+            'Apartamento zona sul do RJ has 3 bedrooms',
+            'Large railroad style 3 bedroom apt in Manhattan! has 3 bedrooms',
+            'Cozy aptartment in Recreio (near Olympic Venues) has 3 bedrooms'
+        ])
+    })
 })
-
-
-
-//   [
-//     'Ribeira Charming Duplex has 3 bedrooms',
-//     'Horto flat with small garden has 1 bedrooms',
-//     'Ocean View Waikiki Marina w/prkg has 1 bedrooms',
-//     'Private Room in Bushwick has 1 bedrooms',
-//     'Apt Linda Vista Lagoa - Rio has 1 bedrooms'
-//   ]
-//   [
-//     'Ribeira Charming Duplex has 3 bedrooms',
-//     '3 chambres au coeur du Plateau has 3 bedrooms',
-//     'Apartamento zona sul do RJ has 3 bedrooms',
-//     'Large railroad style 3 bedroom apt in Manhattan! has 3 bedrooms',
-//     'Cozy aptartment in Recreio (near Olympic Venues) has 3 bedrooms'
-//   ]
 
 
